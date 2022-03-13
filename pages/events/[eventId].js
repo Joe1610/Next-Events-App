944459a2071e56012dfa50763d490b0ffa41e8b3
Joe1610 <joemohammed157@gmail.com>
2022-03-13 17:38:56 +0200

    Adding Event Details Page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,35 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+import EventSummary from "./../../components/event-detail/EventSummary";
+import EventLogistics from "../../components/event-detail/EventLogistics";
+import EventContent from "./../../components/event-detail/EventContent";
+
+import { getEventById } from "../../dummy-data";
+
 function EventDeailsPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Details</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,13 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "./../../components/events/EventList";
+
 function AllEventsPage() {
+  const allEvents = getAllEvents();
+
   return (
     <div>
       <h1>All Events Page</h1>
+      <EventList items={allEvents} />
     </div>
   );
 }
